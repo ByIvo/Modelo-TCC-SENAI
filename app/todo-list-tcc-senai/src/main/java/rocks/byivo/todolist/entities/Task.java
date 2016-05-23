148badbdf2d87a02bda50135df125b0d9e01e283
Ivo Batistela <irbatistela@gmail.com>
2016-05-22 23:37:44 -0300

    first project setup on netbeans

---
+++ app/todo-list-tcc-senai/src/main/java/rocks/byivo/todolist/entities/Task.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rocks.byivo.todolist.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author byivo
+ */
+@Entity
+public class Task implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String mName;
+
+    public Task() {
+
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" + "id=" + id + ", mName=" + mName + '}';
+    }
+
+}
